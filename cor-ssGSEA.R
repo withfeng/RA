40b1ee1c4fa39aa12200bdd689eb4c37f39e574b
withfeng <119863741+withfeng@users.noreply.github.com>
2022-12-05 21:25:01 +0800

    Add files via upload

---
+++ cor-ssGSEA.R
@@ -0,0 +1,81 @@
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("limma")
+
+#install.packages("tidyverse")
+#install.packages("ggplot2")
+#install.packages("reshape2")
+
+
+#引用包
+library(limma)
+library(reshape2)
+library(tidyverse)
+library(ggplot2)
+
+expFile="diffGeneExp.txt"           #表达数据
+immFile="CIBERSORT-Results.txt"     #免疫细胞浸润结果文件
+setwd("C:\\Users\\LENOVO\\Desktop\\cor-CIBRSORT")     #设置工作目录
+
+#读取文件,并对输入文件进行整理
+rt=read.table(expFile, header=T, sep="\t", check.names=F)
+rt=as.matrix(rt)
+rownames(rt)=rt[,1]
+exp=rt[,2:ncol(rt)]
+dimnames=list(rownames(exp),colnames(exp))
+data=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames=dimnames)
+data=avereps(data)
+
+#去除对照组样品
+group=gsub("(.*)\\_(.*)", "\\2", colnames(data))
+data=data[,group=="Treat",drop=F]
+data=t(data)
+
+#读取免疫细胞结果文件，并对数据进行整理
+immune=read.table(immFile, header=T, sep="\t", check.names=F, row.names=1)
+sameSample=intersect(row.names(data), row.names(immune))
+data=data[sameSample,,drop=F]
+immune=immune[sameSample,,drop=F]
+
+#相关性分析
+outTab=data.frame()
+for(cell in colnames(immune)){
+	if(sd(immune[,cell])==0){next}
+	for(gene in colnames(data)){
+		x=as.numeric(immune[,cell])
+		y=as.numeric(data[,gene])
+		corT=cor.test(x,y,method="spearman")
+		cor=corT$estimate
+		pvalue=corT$p.value
+		text=ifelse(pvalue<0.001,"***",ifelse(pvalue<0.01,"**",ifelse(pvalue<0.05,"*","")))
+		outTab=rbind(outTab,cbind(Gene=gene, Immune=cell, cor, text, pvalue))
+	}
+}
+
+#绘制相关性热图
+outTab$cor=as.numeric(outTab$cor)
+pdf(file="cor.pdf", width=7, height=5)
+ggplot(outTab, aes(Immune, Gene)) + 
+	geom_tile(aes(fill = cor), colour = "grey", size = 1)+
+	scale_fill_gradient2(low = "#5C5DAF", mid = "white", high = "#EA2E2D") + 
+	geom_text(aes(label=text),col ="black",size = 3) +
+	theme_minimal() +    #去掉背景
+	theme(axis.title.x=element_blank(), axis.ticks.x=element_blank(), axis.title.y=element_blank(),
+	      axis.text.x = element_text(angle = 45, hjust = 1, size = 8, face = "bold"),   #x轴字体
+	      axis.text.y = element_text(size = 8, face = "bold")) +       #y轴字体
+	labs(fill =paste0("***  p<0.001","\n", "**  p<0.01","\n", " *  p<0.05","\n", "\n","Correlation")) +   #设置图例
+	scale_x_discrete(position = "bottom")      #X轴名称显示位置
+dev.off()
+
+
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
